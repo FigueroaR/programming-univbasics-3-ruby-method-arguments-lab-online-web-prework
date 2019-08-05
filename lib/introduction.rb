fbe1ce09e2728109fb43a318a3d3ff3fa1757674
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-05 21:14:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,9 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, language)
+def introduction_with_language_optional (name,ruby)
   language = "ruby"
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
 def introduction_with_language_optional (name, language)
